ed46bac03a3dd905af235e72320855aa3ba5ddc6
Gleisson Douglas <dhouglassantos@icloud.com>
2026-04-25 23:17:34 -0300

    feat: cria módulos auth e users e instala bcrypt/jsonwebtoken

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const journeysRoutes = require('./modules/journeys/journeys.routes');
-const errorMiddleware = require('./shared/middlewares/error.middleware');
+const journeysRoutes = require("./modules/journeys/journeys.routes");
+const errorMiddleware = require("./shared/middlewares/error.middleware");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.status(200).json({
-        project: 'RotaBus API',
-        status: 'ok',
-        message: 'API rodando com sucesso',
-    });
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    project: "RotaBus API",
+    status: "ok",
+    message: "Tá rodando baby!",
+  });
 });
 
-app.use('/journeys', journeysRoutes);
+app.use("/journeys", journeysRoutes);
 
 app.use(errorMiddleware);
 
